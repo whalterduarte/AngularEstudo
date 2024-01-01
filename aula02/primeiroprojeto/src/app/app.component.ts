ca79b6d2e5a982c6262f87c79bb1ad988773ef9f
Whalter Duarte <dev@whalter.com.br>
2024-01-01 11:57:59 -0300

    Input props

--- aula02/primeiroprojeto/src/app/app.component.ts
+++ aula02/primeiroprojeto/src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FirstComponentComponent],
+  imports: [CommonModule, RouterOutlet, FirstComponentComponent, ParentDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'primeiroprojeto';
+  userName = 'Joaquim'
+  userData = {
+    email: 'Whalter@gamil',
+    role: 'admin'
+  }
 }

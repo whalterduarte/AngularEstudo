9d2561a59a3757f3ceb0642a094df1fcafdc253e
Whalter Duarte <dev@whalter.com.br>
2024-01-02 15:57:51 -0300

    Pipes

--- aula02/primeiroprojeto/src/app/app.component.ts
+++ aula02/primeiroprojeto/src/app/app.component.ts
@@ -6,11 +6,12 @@ import { ParentDataComponent } from './components/parent-data/parent-data.compon
 import { DirectivesComponent } from './components/directives/directives.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent,EmitterComponent, ListRenderComponent],
+  imports: [CommonModule, RouterOutlet, FirstComponentComponent, ParentDataComponent, DirectivesComponent,EmitterComponent, ListRenderComponent, PipesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ aula02/primeiroprojeto/src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pipes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pipes.component.html',
+  styleUrl: './pipes.component.css'
+})
+export class PipesComponent {
+
+}

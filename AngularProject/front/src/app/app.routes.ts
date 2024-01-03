da965217d8c9d8b30212e737a01712ebcee7b159
Whalter Duarte <dev@whalter.com.br>
2024-01-03 18:27:48 -0300

    Header finish

--- AngularProject/front/src/app/app.routes.ts
+++ AngularProject/front/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 
 export const routes: Routes = [
   {path: '', title: 'Home Page', component: HomeComponent},
   {path: 'about', title: 'About', component: AboutComponent},
+  {path: 'moments/new', title: 'Moments', component: NewMomentComponent}
 
 ];
---
+++ AngularProject/front/src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-moment.component.html',
+  styleUrl: './new-moment.component.css'
+})
+export class NewMomentComponent {
+
+}

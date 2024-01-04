1297d1653a55ad229ac7e9d813d16234802ccb11
Whalter Duarte <dev@whalter.com.br>
2024-01-04 16:58:46 -0300

    Interface Moments

--- AngularProject/front/src/app/app.component.ts
+++ AngularProject/front/src/app/app.component.ts
@@ -4,13 +4,13 @@ import { RouterOutlet} from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent,FooterComponent,RouterOutlet, NewMomentComponent],
+  imports: [HttpClientModule, FormsModule, ReactiveFormsModule, CommonModule, HeaderComponent,FooterComponent,RouterOutlet, NewMomentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

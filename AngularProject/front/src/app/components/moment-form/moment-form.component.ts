0f511c3e041b463f4f3cee7b4feb7bde6cbb7fb9
Whalter Duarte <dev@whalter.com.br>
2024-01-05 10:21:57 -0300

    Validator form

--- AngularProject/front/src/app/components/moment-form/moment-form.component.ts
+++ AngularProject/front/src/app/components/moment-form/moment-form.component.ts
@@ -1,13 +1,38 @@
 import { Component, Input } from '@angular/core';
-
+import { FormControl, ReactiveFormsModule, FormGroup, FormsModule, Validator, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-moment-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './moment-form.component.html',
   styleUrl: './moment-form.component.css'
 })
 export class MomentFormComponent {
   @Input() btnText!: string
+
+  momentForm!: FormGroup
+  ngOnInit(): void{
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    })
+  }
+
+  get title() {
+    return this.momentForm.get('title')!
+  }
+  get description() {
+    return this.momentForm.get('description')!
+  }
+
+  submit() {
+    if(this.momentForm.invalid){
+      return
+    }
+    console.log('enviado')
+  }
 }
